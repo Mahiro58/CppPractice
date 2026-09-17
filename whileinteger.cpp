ac3977e0345ac6af5c58db14803bf6e38195ff39
Mahiro58 <mr.shmitu@gmail.com>
2026-09-17 18:33:43 +0200

    int to double update

--- whileinteger.cpp
+++ whileinteger.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main(){
-    int val1 = 0;
-    int val2 = 0;
+    double val1 = 0;
+    double val2 = 0;
     std::cout<<"Podaj dwie liczby calkowie: ";
     while(std::cin>>val1 && std::cin>>val2){
         std::cout<<"Podane liczby to: "<<val1<<" i "<<val2<<'\n';
@@ -13,6 +13,12 @@ int main(){
         else if(val1<val2){
             std::cout<<"Mniejsza liczba to "<<val1<<'\n';
             std::cout<<"Wieksza liczba to "<<val2<<'\n';
+        }
+        else if(val1==val2){
+            std::cout<<"Liczby sa rowne.\n";
+        }
+        else {
+            std::cout<<"Error";
         }
     }
 

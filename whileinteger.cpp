282369b4fea083e7a48a7c99a374884cc88c6300
Mahiro58 <mr.shmitu@gmail.com>
2026-09-17 18:30:46 +0200

    while two integer cout update

--- whileinteger.cpp
+++ whileinteger.cpp
@@ -5,7 +5,15 @@ int main(){
     int val2 = 0;
     std::cout<<"Podaj dwie liczby calkowie: ";
     while(std::cin>>val1 && std::cin>>val2){
-        std::cout<<val1<<","<<val2;
+        std::cout<<"Podane liczby to: "<<val1<<" i "<<val2<<'\n';
+        if(val1>val2){
+            std::cout<<"Mniejsza liczba to "<<val2<<'\n';
+            std::cout<<"Wieksza liczba to "<<val1<<'\n';
+        }
+        else if(val1<val2){
+            std::cout<<"Mniejsza liczba to "<<val1<<'\n';
+            std::cout<<"Wieksza liczba to "<<val2<<'\n';
+        }
     }
 
     return 0;

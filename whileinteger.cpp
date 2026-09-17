dbae2793848367c9ad98dc21aeffc44f9d9e9bae
Mahiro58 <mr.shmitu@gmail.com>
2026-09-17 18:42:37 +0200

    absolute value

--- whileinteger.cpp
+++ whileinteger.cpp
@@ -9,10 +9,16 @@ int main(){
         if(val1>val2){
             std::cout<<"Mniejsza liczba to "<<val2<<'\n';
             std::cout<<"Wieksza liczba to "<<val1<<'\n';
+            if (std::abs(val1-val2)<0.01){
+                std::cout<<"Liczby sa prawie rowne.\n";
+            }
         }
         else if(val1<val2){
             std::cout<<"Mniejsza liczba to "<<val1<<'\n';
             std::cout<<"Wieksza liczba to "<<val2<<'\n';
+            if (std::abs(val2-val1)<0.01){
+                std::cout<<"Liczby sa prawie rowne.\n";
+            }
         }
         else if(val1==val2){
             std::cout<<"Liczby sa rowne.\n";

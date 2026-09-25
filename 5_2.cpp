5ec62b2e02589e70e8a4e8ba7201c45be12d9ca3
Mahiro58 <mr.shmitu@gmail.com>
2026-09-25 11:18:25 +0200

    expression update

--- 5_2.cpp
+++ 5_2.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 
-class Token{
+class Token
+{
     public:
         char kind;
         double value;
@@ -10,6 +11,75 @@ class Token{
 };
 Token get_token();
 std::vector<Token> tok;
+
+double expression()
+{
+    double left = term();
+    Token t = get_token();
+    while(true) 
+    {
+        switch (t.kind)
+        {
+        case '+':
+            left += term();
+            t = get_token();
+            break;
+
+        case '-':
+            left -= term();
+            t = get_token();
+            break;
+        
+        default:
+            return left;
+        }
+    }
+}
+
+double term()
+{
+    double left = primary();
+    Token t = get_token();
+    while(true)
+    {
+        switch (t.kind)
+        {
+        case '*':
+            left *= primary();
+            t = get_token();
+            break;
+
+        case '/':
+        {
+            double d = primary();
+            if(d == 0) std::cerr<<"Dzielnie przez zero!\n";
+            left /= primary();
+            t = get_token();
+        }
+        default:
+            return left;
+        }
+    }
+}
+
+double primary()
+{
+    Token t = get_token();
+    switch (t.kind)
+    {
+    case '()':
+    {
+        double d = expression();
+        t = get_token();
+        if(t.kind != ')') std::cerr<<"Oczekiwano ')'\n";
+        return d;
+    }
+    case '8':
+        return t.value;
+    default:
+        std::cerr<<"Oczekiwano czynnika.\n";
+    }
+}
 
 int main()
 {
